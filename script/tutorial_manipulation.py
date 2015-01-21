0732a4da5351dc49ba8c16e54c6c7f46dbe9a096
Joseph Mirabel <jmirabel@laas.fr>
2015-01-21 16:22:46 +0100

    Fix script tutorial_manipulation.py

--- script/tutorial_manipulation.py
+++ script/tutorial_manipulation.py
@@ -3,26 +3,39 @@
 # Import. {{{2
 from hpp.corbaserver.manipulation.pr2 import Robot
 from hpp.corbaserver.manipulation import ProblemSolver, ConstraintGraph
-from hpp_ros.manipulation import ScenePublisher
+from hpp.gepetto.manipulation import Viewer, FakeViewer
 # 2}}}
 
 # Load PR2 and a box to be manipulated. {{{2
-robot = Robot ('pr2')
-robot.loadObjectModel ('box', 'freeflyer', 'hpp_tutorial', 'box', '', '')
-robot.buildCompositeRobot ('pr2-box', ['pr2', 'box'])
-robot.client.manipulation.robot.loadEnvironmentModel ("iai_maps", "kitchen_area", "", '', '')
+class Box (object):
+  rootJointType = 'freeflyer'
+  packageName = 'hpp_tutorial'
+  meshPackageName = 'hpp_tutorial'
+  urdfName = 'box'
+  urdfSuffix = ""
+  srdfSuffix = ""
+
+class Environment (object):
+  packageName = 'iai_maps'
+  meshPackageName = 'iai_maps'
+  urdfName = 'kitchen_area'
+  urdfSuffix = ""
+  srdfSuffix = ""
+
+robot = Robot ('pr2-box', 'pr2')
+ps = ProblemSolver (robot)
+fk = FakeViewer (ps)
+
+fk.loadObjectModel (Box, 'box')
+fk.buildCompositeRobot (['pr2', 'box'])
+fk.loadEnvironmentModel (Environment, "kitchen_area")
 
 robot.setJointBounds ("pr2/base_joint_xy" , [-5,-2,-5.2,-2.7]     )
 robot.setJointBounds ("box/base_joint_xyz", [-5.1,-2,-5.2,-2.7,0,1.5])
 # 2}}}
 
-# Load the Python class ProblemSolver and ConstraintGraph. {{{2
-p = ProblemSolver (robot)
+# Load the Python class ConstraintGraph. {{{2
 graph = ConstraintGraph (robot, 'graph')
-# 2}}}
-
-# Load the ScenePublisher. {{{2
-r = ScenePublisher (robot)
 # 2}}}
 
 # 1}}}
@@ -46,6 +59,8 @@ for n in jointNames['all']:
     jointNames['pr2'].append (n)
   if not n.startswith ("pr2/l_gripper"):
     jointNames['allButPR2LeftArm'].append (n)
+
+ps.addPassiveDofs ('pr2', jointNames['pr2'])
 # 2}}}
 
 # Generate initial and goal configuration. {{{2
@@ -60,7 +75,6 @@ rank = robot.rankInConfiguration ['pr2/torso_lift_joint']
 q_init [rank] = 0.2
 rank = robot.rankInConfiguration ['box/base_joint_xyz']
 q_init [rank:rank+3] = [-2.5, -4, 0.8]
-r (q_init)
 
 q_goal [0:2] = [-3.2, -4]
 rank = robot.rankInConfiguration ['pr2/l_shoulder_lift_joint']
@@ -75,22 +89,19 @@ rank = robot.rankInConfiguration ['box/base_joint_xyz']
 q_goal [rank:rank+3] = [-4.8, -4.6, 0.9]
 rank = robot.rankInConfiguration ['box/base_joint_SO3']
 q_goal [rank:rank+4] = [0, 0, 0, 1]
-r (q_goal)
 # 2}}}
 
 # Create the constraints. {{{2
-graph.createGrasp ('l_grasp', 'pr2/l_gripper', 'box/handle', passiveJoints = jointNames['pr2'])
+graph.createGrasp ('l_grasp', 'pr2/l_gripper', 'box/handle', passiveJoints = 'pr2')
 graph.createPreGrasp ('l_pregrasp', 'pr2/l_gripper', 'box/handle')
 
-lockbox = p.lockFreeFlyerJoint ('box/base_joint', 'box_lock')
-lockpr2 = p.lockPlanarJoint ('pr2/base_joint', 'pr2_lock')
+lockbox = ps.lockFreeFlyerJoint ('box/base_joint', 'box_lock', compType = 'Equality')
+lockpr2 = ps.lockPlanarJoint ('pr2/base_joint', 'pr2_lock', compType = 'Equality')
 lockboth = lockpr2[:]; lockboth.extend (lockbox)
 
 locklhand = ['l_l_finger','l_r_finger'];
-p.createLockedJointConstraint ('l_l_finger', 'pr2/l_gripper_l_finger_joint',
-                               [0.5,])
-p.createLockedJointConstraint ('l_r_finger', 'pr2/l_gripper_r_finger_joint',
-                               [0.5,])
+ps.createLockedJoint ('l_l_finger', 'pr2/l_gripper_l_finger_joint', [0.5,])
+ps.createLockedJoint ('l_r_finger', 'pr2/l_gripper_r_finger_joint', [0.5,])
 # 2}}}
 
 # Create the constraint graph. {{{2
@@ -112,18 +123,18 @@ graph.setConstraints (node='box', grasp='l_grasp')
 graph.setConstraints (edge='move_free', lockDof = lockbox)
 graph.setConstraints (edge="ungrasp_e1", lockDof = lockbox)
 graph.setConstraints (node="ungrasp_n0", pregrasp = 'l_pregrasp')
-graph.setConstraints (edge="ungrasp_e0", lockDof = lockboth)
-graph.setConstraints (edge="grasp_e1", lockDof = lockboth)
+graph.setConstraints (edge="ungrasp_e0", lockDof = lockbox)
+graph.setConstraints (edge="grasp_e1", lockDof = lockbox)
 graph.setConstraints (node="grasp_n0", pregrasp = 'l_pregrasp')
 graph.setConstraints (edge="grasp_e0", lockDof = lockbox)
-graph.client.graph.setLevelSetConstraints  (graph.edges["keep_grasp_ls"], [], lockbox)
+graph.setLevelSetConstraints ("keep_grasp_ls", lockDof = lockbox)
 graph.setConstraints (graph = True, lockDof = locklhand)
 # 3}}}
 
 # 2}}}
 
-p.setInitialConfig (q_init)
-p.addGoalConfig (q_goal)
+ps.setInitialConfig (q_init)
+ps.addGoalConfig (q_goal)
 
 # 1}}}
 
