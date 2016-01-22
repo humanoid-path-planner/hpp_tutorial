38b12767a67044b1121c0f522f6641565b73b6d2
Pierre Fernbach <pierre.fernbach@laas.fr>
2016-01-22 10:10:56 +0100

    show use of the function with default value and add comments

--- script/tutorial_1_roadmap.py
+++ script/tutorial_1_roadmap.py
@@ -3,11 +3,13 @@ from hpp.corbaserver import ProblemSolver
 from hpp.gepetto import Viewer
 from hpp.gepetto import PathPlayer
 
-# define colors for the roadmap
+# define colors for the roadmap (normalized RGBA)
 white=[1.0,1.0,1.0,1.0]
-green=[0.23,0.75,0.2,0.5]
-brown=[0.85,0.75,0.15,0.5]
+lightGreen=[0.23,0.75,0.2,0.5]
+green=[0.23,0.75,0.2,1]
+yellow=[0.85,0.75,0.15,0.7]
 blue = [0.0, 0.0, 0.8, 1.0]
+lightBlue = [0.0, 0.0, 0.8, 0.7]
 grey = [0.7,0.7,0.7,1.0]
 red = [0.8,0.0,0.0,1.0]
 
@@ -32,39 +34,43 @@ rank = robot.rankInConfiguration ['r_shoulder_lift_joint']
 q_goal [rank] = 0.5
 rank = robot.rankInConfiguration ['r_elbow_flex_joint']
 q_goal [rank] = -0.5
-#r (q_goal)
+#v (q_goal)
 
 v.loadObstacleModel ("iai_maps", "kitchen_area", "kitchen")
 ps.setInitialConfig (q_init)
 ps.addGoalConfig (q_goal)
 
-#ps.selectPathPlanner("rrtConnect")
-
 ps.solve ()
 
 # display roadmap for the base of the robot (no specified joint)
-v.displayRoadmap("rmB",white,0.02,1,green)
-# display the path found in the roadmap : 
-v.displayPathMap("rmPath",0,red,0.03,1,red)
+# displayRoadmap(name,sizeNode)
+v.displayRoadmap("rmB",0.02)
+# display the path found in the roadmap :
+# displayPathMap(name,pathID,sizeNode) or : 
+# displayPathMap(name,pathID,sizeNode,sizeAxis,colorNode,colorEdge,JointName)
+v.displayPathMap("rmPath",0,0.03)
 # hide previous roadmap
 v.client.gui.removeFromGroup("rmB",v.sceneName)
 v.client.gui.removeFromGroup("rmPath",v.sceneName)
 
 
-#display roadmap for the tools :
-v.displayRoadmap("rmR",blue,0.02,1,green,'r_gripper_tool_joint')
-v.displayRoadmap("rmL",red,0.02,1,grey,'l_gripper_tool_joint')
+# display roadmap for the tools, the full prototype is :
+# displayRoadmap(name,sizeNode,sizeAxis,colorNode,colorEdge,JointName)
+# The joint defined by "jointName" (see robot.getAllJointNames()) is used to compute the position of the node of the roadmap 
+v.displayRoadmap("rmR",0.02,1,blue,lightBlue,'r_gripper_tool_joint')
+v.displayRoadmap("rmL",0.02,1,green,lightGreen,'l_gripper_tool_joint')
 
 
 # alternative method : replace ps.solve() and v.displayRoadmap() with :
-# v.solveAndDisplay("rmR",2,blue,0.02,1,green,'r_gripper_tool_joint')
-# v.displayRoadmap("rmL",red,0.02,1,grey,'l_gripper_tool_joint')
+# v.solveAndDisplay("rmL",2,0.02,1,green,lightGreen,'l_gripper_tool_joint')
+# v.displayRoadmap("rmR",0.02,1,blue,lightBlue,'r_gripper_tool_joint')
+# solveAndDisplay show the construction of the roadmap, the first int set the refresh rate of the display (here, every 2 iteration of the algorithm)
 ################################################################
 
 pp = PathPlayer (robot.client, v)
 #display path
 pp (0)
-#display path with post-optimisation
+#display path after optimisation
 pp (1)
 
 
