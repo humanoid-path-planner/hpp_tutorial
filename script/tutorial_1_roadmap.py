51b9488e378c7785a5e4252ba8243c30e60525b2
Pierre Fernbach <pierre.fernbach@laas.fr>
2016-01-29 10:33:24 +0100

    align function prototype for better explanation in comments

--- script/tutorial_1_roadmap.py
+++ script/tutorial_1_roadmap.py
@@ -7,7 +7,7 @@ from hpp.gepetto import PathPlayer
 white=[1.0,1.0,1.0,1.0]
 lightGreen=[0.23,0.75,0.2,0.5]
 green=[0.23,0.75,0.2,1]
-yellow=[0.85,0.75,0.15,0.7]
+yellow=[0.85,0.75,0.15,0.9]
 blue = [0.0, 0.0, 0.8, 1.0]
 lightBlue = [0.0, 0.0, 0.8, 0.7]
 grey = [0.7,0.7,0.7,1.0]
@@ -39,6 +39,7 @@ q_goal [rank] = -0.5
 v.loadObstacleModel ("iai_maps", "kitchen_area", "kitchen")
 ps.setInitialConfig (q_init)
 ps.addGoalConfig (q_goal)
+ps.addPathOptimizer ("RandomShortcut")
 
 ps.solve ()
 
@@ -63,14 +64,17 @@ v.displayRoadmap("rmL",0.02,1,green,lightGreen,'l_gripper_tool_joint')
 
 # alternative method : replace ps.solve() and v.displayRoadmap() with :
 # v.solveAndDisplay("rmL",2,0.02,1,green,lightGreen,'l_gripper_tool_joint')
-# v.displayRoadmap("rmR",0.02,1,blue,lightBlue,'r_gripper_tool_joint')
+# v.displayRoadmap( "rmR"  ,0.02,1,blue ,lightBlue ,'r_gripper_tool_joint')
 # solveAndDisplay show the construction of the roadmap, the first int set the refresh rate of the display (here, every 2 iteration of the algorithm)
 ################################################################
 
 pp = PathPlayer (robot.client, v)
 #display path
+# display the curve of the path (default color = yellow)
+pp.displayPath(0,jointName='r_gripper_tool_joint')
 pp (0)
 #display path after optimisation
+pp.displayPath(1,yellow,'r_gripper_tool_joint')
 pp (1)
 
 
