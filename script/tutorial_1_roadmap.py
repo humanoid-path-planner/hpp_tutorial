43f78326e303dff26f3c8124a3cc138a8a591930
Pierre Fernbach <pierre.fernbach@laas.fr>
2016-01-29 13:09:19 +0100

    Move definition of color in Viewer class, add comments

--- script/tutorial_1_roadmap.py
+++ script/tutorial_1_roadmap.py
@@ -3,15 +3,7 @@ from hpp.corbaserver import ProblemSolver
 from hpp.gepetto import Viewer
 from hpp.gepetto import PathPlayer
 
-# define colors for the roadmap (normalized RGBA)
-white=[1.0,1.0,1.0,1.0]
-lightGreen=[0.23,0.75,0.2,0.5]
-green=[0.23,0.75,0.2,1]
-yellow=[0.85,0.75,0.15,0.9]
-blue = [0.0, 0.0, 0.8, 1.0]
-lightBlue = [0.0, 0.0, 0.8, 0.7]
-grey = [0.7,0.7,0.7,1.0]
-red = [0.8,0.0,0.0,1.0]
+
 
 robot = Robot ('pr2')
 robot.setJointBounds ("base_joint_xy", [-4, -3, -5, -3])
@@ -44,7 +36,7 @@ ps.addPathOptimizer ("RandomShortcut")
 ps.solve ()
 
 # display roadmap for the base of the robot (no specified joint)
-# displayRoadmap(name,sizeNode)
+# displayRoadmap(name ,sizeNode)
 v.displayRoadmap("rmB",0.02)
 # display the path found in the roadmap :
 # displayPathMap(name,pathID,sizeNode) or : 
@@ -57,28 +49,34 @@ v.client.gui.removeFromGroup("rmPath",v.sceneName)
 
 # display roadmap for the tools, the full prototype is :
 # displayRoadmap(name,sizeNode,sizeAxis,colorNode,colorEdge,JointName)
-# The joint defined by "jointName" (see robot.getAllJointNames()) is used to compute the position of the node of the roadmap 
-v.displayRoadmap("rmR",0.02,1,blue,lightBlue,'r_gripper_tool_joint')
-v.displayRoadmap("rmL",0.02,1,green,lightGreen,'l_gripper_tool_joint')
+# The joint defined by "jointName" (see robot.getAllJointNames()) is used to compute the position of the node of the roadmap
+# You can use the colors defined in viewer.color or give an array of 4 float which define a normalized RGBA color 
+v.displayRoadmap("rmR",0.02,1,v.color.blue,v.color.lightBlue,'r_gripper_tool_joint')
+v.displayRoadmap("rmL",0.02,1,v.color.green,v.color.lightGreen,'l_gripper_tool_joint')
+
+
+# hide roadmap in the scene
+#v.client.gui.removeFromGroup("rmL",v.sceneName)
+#v.client.gui.removeFromGroup("rmR",v.sceneName)
+
 
 
 # alternative method : replace ps.solve() and v.displayRoadmap() with :
-# v.solveAndDisplay("rmL",2,0.02,1,green,lightGreen,'l_gripper_tool_joint')
-# v.displayRoadmap( "rmR"  ,0.02,1,blue ,lightBlue ,'r_gripper_tool_joint')
+# v.solveAndDisplay("rmL1",2,0.02,1,v.color.green,v.color.lightGreen,'l_gripper_tool_joint')
+# v.displayRoadmap( "rmR1"  ,0.02,1,v.color.blue ,v.color.lightBlue ,'r_gripper_tool_joint')
 # solveAndDisplay show the construction of the roadmap, the first int set the refresh rate of the display (here, every 2 iteration of the algorithm)
 ################################################################
 
 pp = PathPlayer (robot.client, v)
 #display path
-# display the curve of the path (default color = yellow)
-pp.displayPath(0,jointName='r_gripper_tool_joint')
+# The edge of the roadmap are always straight line, if you want to display the accurate trajectory of a body of the robot, you should use the following method :
+#  displayPath(pathId, color(default = green), jointName (default = root)
+pp.displayPath(0     , v.color.lightBlack    ,'r_gripper_tool_joint')
 pp (0)
 #display path after optimisation
-pp.displayPath(1,yellow,'r_gripper_tool_joint')
+pp.displayPath(1,v.color.lightRed,'r_gripper_tool_joint')
 pp (1)
 
 
-# hide roadmap in the scene
-v.client.gui.removeFromGroup("rmL",v.sceneName)
-v.client.gui.removeFromGroup("rmR",v.sceneName)
+
 
