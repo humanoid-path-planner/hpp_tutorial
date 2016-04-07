7616b54ed6b04fac356372649750e3ada123001a
Joseph Mirabel <jmirabel@laas.fr>
2016-04-07 18:58:58 +0200

    Fix .travis/script.py

--- .travis/script.py
+++ .travis/script.py
@@ -5,15 +5,16 @@ robot.setJointBounds ("base_joint_xy", [-4, -3, -5, -3])
 from hpp.corbaserver import ProblemSolver
 ps = ProblemSolver (robot)
 
-from hpp.gepetto import ViewerFactory
-r = ViewerFactory (ps)
+# On travis, we do not compile the viewer (yet?)
+# from hpp.gepetto import ViewerFactory
+# r = ViewerFactory (ps)
 
 q_init = robot.getCurrentConfig ()
 q_goal = q_init [::]
 q_init [0:2] = [-3.2, -4]
 rank = robot.rankInConfiguration ['torso_lift_joint']
 q_init [rank] = 0.2
-r (q_init)
+# r (q_init)
 
 q_goal [0:2] = [-3.2, -4]
 rank = robot.rankInConfiguration ['l_shoulder_lift_joint']
@@ -24,9 +25,10 @@ rank = robot.rankInConfiguration ['r_shoulder_lift_joint']
 q_goal [rank] = 0.5
 rank = robot.rankInConfiguration ['r_elbow_flex_joint']
 q_goal [rank] = -0.5
-r (q_goal)
+# r (q_goal)
 
-r.loadObstacleModel ("iai_maps", "kitchen_area", "kitchen")
+# r.loadObstacleModel ("iai_maps", "kitchen_area", "kitchen")
+ps.loadObstacleFromUrdf ("iai_maps", "kitchen_area", "kitchen/")
 
 ps.setInitialConfig (q_init)
 ps.addGoalConfig (q_goal)
